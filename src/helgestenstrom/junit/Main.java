ff6af7ca47f4e99c15ba17b1eed4e55c3f1d72f2
Helge Stenström <h.stenstrom@gmail.com>
2017-09-26 21:16:19 +0200

    assert bytt mot assertThat()

--- src/helgestenstrom/junit/Main.java
+++ src/helgestenstrom/junit/Main.java
@@ -1,41 +1,62 @@
 package helgestenstrom.junit;
 
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
         testThatRunInWasRunMarksThatItWasRun();
         testCanBeTestCase();
         classHasAName();
         callMethodByName();
-        findMethodsThatAreTests();
+        compareStringLists();
+        // findMethodsThatAreTests();
     }
 
     private static void testThatRunInWasRunMarksThatItWasRun() {
         WasRun test = new WasRun("testMethod");
-        assert test.wasRun == false;
+        assertThat(!test.wasRun);
         test.run();
-        myAssert(test.wasRun);
+        assertThat(test.wasRun);
     }
 
     private static void testCanBeTestCase() {
         TestCase test = new WasRun("testMethod");
-        assert test.wasRunParent = false;
+        assertThat(!test.wasRunParent);
         test.run();
-        myAssert(test.wasRunParent);
+        assertThat(test.wasRunParent);
     }
 
     private static void classHasAName() {
         TestCase test = new WasRun("testMethod");
-        myAssert("testMethod".equals(test.nameInBaseClass));
+        assertThat("testMethod".equals(test.nameInBaseClass));
     }
 
     private static void callMethodByName() {
         TestCase test = new WasRun("testMethod");
-        assert test.wasRunParent = false;
+        assertThat(!test.wasRunParent);
         test.runByName();
-        myAssert(test.wasRunParent);
+        assertThat(test.wasRunParent);
         System.out.print(".");
+    }
+
+    private static void compareStringLists() {
+        ArrayList<String> l1 = new ArrayList<String>();
+        ArrayList<String> l2 = new ArrayList<String>();
+        l1.add("One");
+        l1.add("Two");
+        l1.add("Three");
+        l2.addAll(l1);
+        assertThat(l1.equals(l1));
+        assertThat(l1.equals(l2));
+        l2.add("Four");
+        assertThat(l1.contains("Two"));
+        assertThat(!(l1.contains("Four")));
+        assertThat(l2.contains("Four"));
+        assertThat(l1.size() == 3);
+        assertThat(l2.size() == 4);
     }
 
     private static void findMethodsThatAreTests() {
@@ -43,10 +64,11 @@ public class Main {
 
         // TODO: lista över namn, ska jämföras med list från testFinder
 
+        throw new NotImplementedException();
 
     }
 
-    private static void myAssert(boolean b) {
+    private static void assertThat(boolean b) {
         if (!b) {
             throw new AssertionError("Not true ");
         }
